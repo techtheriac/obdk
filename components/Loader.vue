3b873dac9891e162dc2101eb979b086c4991f695
Jezreel Franklin <mavyjayann@gmail.com>
2024-01-18 18:22:45 +0100

    fix techtheriac container padding

--- components/Loader.vue
+++ components/Loader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="loader">
+  <div class="loader fill-height">
     <div>
       <Techtheriac :is-flip-destination="false" />
     </div>
@@ -10,9 +10,8 @@
 
 <style lang="scss" scoped>
 .loader {
-  --size-background: 5rem;
-  height: 100dvh;
-  width: 100vw;
+  --size-background: var(--size-background-base);
+  width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -21,8 +20,6 @@
   top: 0;
   left: 0;
   background-color: var(--background-dark-07);
-  padding-left: var(--space-s);
-  padding-right: var(--space-s);
   background-image: linear-gradient(
       to right,
       var(--foreground-dark-forest) 1px,
@@ -37,6 +34,8 @@
 
   div {
     position: absolute;
+    padding-right: var(--space-xs);
+    padding-left: var(--space-xs);
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
