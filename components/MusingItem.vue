39d31e7853fec9e324debbe7f5868004c9b23deb
Jezreel Franklin <mavyjayann@gmail.com>
2024-03-25 13:20:08 +0100

    Update date & time components

--- components/MusingItem.vue
+++ components/MusingItem.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="musing__item musing__item__layout">
+    <div>
+      <span>{{ genre }}</span>
+    </div>
     <div class="meta meta__layout">
-      <Rounded :genre="genre" />
-      <span>{{ publised }}</span>
+      <span class="published">{{ publishedText }}</span>
     </div>
     <div class="title title__layout">
       <NuxtLink :to="path">
@@ -13,24 +15,36 @@
 </template>
 
 <script setup lang="ts">
+import { parseISO, format } from "date-fns";
 const props = defineProps<{
   title?: string;
   path?: string;
   genre: string;
-  publised?: string;
+  published?: string;
   stage?: string;
 }>();
+
+const publishedText = computed(() => {
+  if (!props.published) return;
+
+  const parsedDate = parseISO(props.published);
+  const date = format(parsedDate, "do MMM yyyy");
+  const time = format(parsedDate, "h:mm a");
+
+  return `last updated on ${date} at ${time}`;
+});
 </script>
 
 <style scoped lang="scss">
 .musing__item {
-  display: grid;
   padding: var(--space-s) 0 var(--space-s) 0;
   width: 100%;
   border-top: 1px solid #ffffff91;
 
   &__layout {
     grid-template-columns: repeat(6, 1fr);
+    display: flex;
+    flex-direction: column;
   }
 }
 
@@ -47,7 +61,7 @@ const props = defineProps<{
   span:nth-child(1) {
     font-weight: 400;
     padding-right: var(--space-xs);
-    border-right: 1px solid #fff;
+    // border-right: 1px solid #fff;
   }
 
   span:nth-child(2) {
@@ -55,31 +69,20 @@ const props = defineProps<{
   }
 }
 .title {
-  width: min(100%, 50ch);
+  width: max(100%, 75ch);
 
   &__layout {
     grid-column: 3 / -1;
   }
 
   a {
-    font-size: var(--idealHeadingTwo);
+    font-size: var(--idealHeadingOne);
     color: #fff;
   }
 }
 
-@media screen and (max-width: 800px) {
-  .musing__item__layout {
-    grid-template-rows: auto 1fr;
-    gap: var(--space-s);
-  }
-  .meta__layout {
-    grid-column: 1 / -1;
-    grid-row: 1 / 2;
-  }
-
-  .title__layout {
-    grid-column: 1 / -1;
-    grid-row: 2 / -1;
-  }
+.published {
+  font-family: "Space Mono";
+  font-weight: 400;
 }
 </style>
