d54c3a7237d835d3f8fd9f6f52b6aac946b7e6be
Jezreel Franklin <mavyjayann@gmail.com>
2024-04-19 21:09:42 +0100

    Add label to nav

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -12,6 +12,8 @@
         d="M1 12c6.012.536 10.105 1.364 12.282 2.484 2.177 1.12 4.557 3.626 7.142 7.516M72 12H1h71ZM1 12c6.012-.536 10.105-1.364 12.282-2.484C15.459 8.396 17.839 5.89 20.424 2L1 12Z"
       />
     </svg>
+
+    <span>{{ text }}</span>
   </NuxtLink>
 </template>
 
@@ -23,9 +25,13 @@ const props = defineProps<{
 </script>
 <style lang="scss" scoped>
 .navigation {
-  height: 40px;
-  width: 40px;
   position: relative;
-  padding: var(--space) 0;
+  padding: var(--space-s) 0;
+  color: rgba(255, 255, 255, 0.63);
+  font-weight: normal;
+  display: inline-flex;
+  align-items: center;
+  text-transform: uppercase;
+  font-size: var(--idealSubFontSize);
 }
 </style>
