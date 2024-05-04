5f25ef1b90c5b3300c67fb29feb0357cc6bf8e66
Jezreel Franklin <mavyjayann@gmail.com>
2024-05-04 19:39:25 +0100

    specify lang attr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
     head: {
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
+      title: "Jezreel Franklin | Software Engineer",
+      htmlAttrs: {
+        lang: "en",
+      },
     },
   },
   typescript: {
