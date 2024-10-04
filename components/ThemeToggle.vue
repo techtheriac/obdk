846d6592906b2330c92a5c7c0d502508570976fe
Jezreel Franklin <mavyjayann@gmail.com>
2024-10-04 20:10:14 +0100

    Add theme toggle

---
+++ components/ThemeToggle.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="theme-toggle" role="button">
+    <span></span>
+    <span></span>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.theme-toggle {
+  --size: 2ch;
+  --half: calc(var(--size) / 2);
+  display: flex;
+  gap: 2px;
+  font-size: var(--step-0);
+
+  span {
+    height: var(--size);
+    width: var(--half);
+  }
+
+  span:nth-child(1) {
+    border-radius: var(--half) 0 0 var(--half);
+    background-color: var(--elevated-dark);
+  }
+
+  span:nth-child(2) {
+    border-radius: 0 var(--half) var(--half) 0;
+    background-color: var(--elevated-light);
+  }
+}
+</style>
