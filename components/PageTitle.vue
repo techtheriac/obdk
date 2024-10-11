abf7aa1a7708561b3f0882153d4e2477c03460ce
Jezreel Franklin <mavyjayann@gmail.com>
2024-10-11 18:47:21 +0100

    Add default title component for pages

---
+++ components/PageTitle.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page-title">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  title: string;
+}>();
+</script>
+
+<style scoped lang="scss">
+.page-title {
+  padding-block: var(--space-s);
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    font-family: "Grotesque";
+    font-size: var(--step-2);
+    text-transform: capitalize;
+  }
+}
+</style>
