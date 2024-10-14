5fbe3021ae64e2f310f18972af9bca13d69cb900
Jezreel Franklin <mavyjayann@gmail.com>
2024-10-14 14:25:09 +0100

    Initiate loading animation in main layout

--- layouts/main.vue
+++ layouts/main.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="layout-base">
+  <div class="layout-main">
     <Techtheriac :should-blink="true" :should-animate="true" />
     <main class="main">
       <slot />
@@ -8,4 +8,29 @@
   </div>
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import { type LandingAnimatable } from "~/utils/animations/landing";
+import { Application } from "~/utils/application";
+
+onBeforeMount(() => {
+  const animatables: LandingAnimatable = {
+    techtheriac: document.querySelector(".obdk")!,
+    bio: document.querySelector(".bio")!,
+    nowSlider: document.querySelector(".now-slider")!,
+    flipContainer: document.querySelector(".flip-container")!,
+    navigation: document.querySelector(".navigation")!,
+    header: document.querySelector(".header")!,
+  };
+
+  new Application(animatables);
+});
+</script>
+
+<style scoped lang="scss">
+.layout-main {
+  display: flex;
+  flex-direction: column;
+  color: var(--foreground-100);
+  padding-inline: var(--space-xs);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,6 @@
 <template>
-  <main>
-    <Intro />
-    <SubmissionsIndex />
-    <LabFeatured />
-    <PreFooter />
-  </main>
+  <Intro />
+  <SubmissionsIndex />
+  <LabFeatured />
+  <PreFooter />
 </template>
-
-<script setup lang="ts">
-import { type LandingAnimatable } from "~/utils/animations/landing";
-import { Application } from "~/utils/application";
-
-definePageMeta({
-  layout: "main",
-});
-
-onMounted(() => {
-  const animatables: LandingAnimatable = {
-    techtheriac: document.querySelector(".obdk")!,
-    bio: document.querySelector(".bio")!,
-    nowSlider: document.querySelector(".now-slider")!,
-    flipContainer: document.querySelector(".flip-container")!,
-    navigation: document.querySelector(".navigation")!,
-    header: document.querySelector(".header")!,
-  };
-
-  new Application(animatables);
-});
-</script>
-<style></style>
